8f927cda639768051d33ffe5880842bd32bbe869
Tabije-9ruby-ay2526 <tabije.ae@obmontessori.edu.ph>
2026-03-13 13:51:33 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function verbNoun(){
+    const ADMIN_FEE = 50.00;
+    const TICKET_PRICE = 250;
+    let subTotal = TICKET_PRICE * quantity;
+    let Total= subTotal + ADMIN_FEE;
+    let quantity = document.getElementById("quantity").value;
+}
+
